d4061332d063f2f10e9142cb0791507f632fb50e
GesChen <omgtntlol101@gmail.com>
2024-07-12 23:10:41 -0400

    added funny group picture
    + failed mute button attempt thing i hate google why can you fIJKFSDklhing add autoplay and not mute + why wont the FUHCKHFKING

---
+++ toggles.js
@@ -0,0 +1,15 @@
+// im aware this is a very bad way of doing it but shut up i dont have time for a better solution
+
+const videoElements = document.querySelectorAll("video");
+console.log(videoElements);
+videoElements.forEach(function(video) {
+    console.log("doing "+video.id);
+
+    // Create a hidden checkbox toggle with the same ID
+    const muteToggle = document.getElementById("cb"+video.id);
+
+    // Attach the toggle functionality
+    muteToggle.addEventListener("change", function() {
+        video.muted = muteToggle.checked;
+    });
+});
